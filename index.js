5b560ca2f6375db0abfc4572c1201bae72adfbbd
onnanoco <moe@onnano.co>
2022-02-18 06:11:48 +0900

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+const { constants } = require("./lib/constants.js");
+
 app.get("/url", (req, res, next) => {
-    res.json(["sample"]);
+    res.json([constants.MOE_CONTRACT_ADDRESS]);
 });
 
 app.listen(3000, () => {
